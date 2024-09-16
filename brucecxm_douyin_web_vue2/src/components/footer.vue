8a909c7a513ebce6b79e671cc0e1432c12d85edb
Bruce chen <130543450+brucecxm@users.noreply.github.com>
2024-09-16 15:38:27 +0800

    1
    1

--- brucecxm_douyin_web_vue2/src/components/footer.vue
+++ brucecxm_douyin_web_vue2/src/components/footer.vue
@@ -1,22 +1,36 @@
 <template>
     <div class="footer">
-        <div class="home nav">首页</div>
-        <div class="shop nav">商城</div>
-        <div class="add nav">＋</div>
-        <div class="message nav">消息</div>
-        <div class="me nav">我</div>
+        <div class="nav home" :class="{ active: selected === 'home' }" @click="select('home')">
+            首页
+        </div>
+        <div class="nav shop" :class="{ active: selected === 'shop' }" @click="select('shop')">
+            商城
+        </div>
+        <div class="nav add" :class="{ active: selected === 'add' }" @click="select('add')">
+            ＋
+        </div>
+        <div class="nav message" :class="{ active: selected === 'message' }" @click="select('message')">
+            消息
+        </div>
+        <div class="nav me" :class="{ active: selected === 'me' }" @click="select('me')">
+            我
+        </div>
     </div>
 </template>
 
 <script>
-
 export default {
     data() {
         return {
-
+            selected: '', // 当前选中的选项
         };
     },
-
+    methods: {
+        select(option) {
+            this.selected = option;
+            this.$emit('selection-changed', option); // 向父组件传递数据
+        }
+    }
 };
 </script>
 
@@ -25,7 +39,6 @@ export default {
     width: 100%;
     height: 2rem;
     background-color: black;
-
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -34,7 +47,16 @@ export default {
 
 .nav {
     font-size: 0.7rem;
-    color: white;
+    color: rgba(255, 255, 255, 0.9);
+    margin: 0.5rem;
+    cursor: pointer;
+    /* 添加鼠标光标效果 */
+    transition: color 0.3s ease;
+    /* 平滑过渡效果 */
+}
 
+.nav.active {
+    color: #f5f5f5;
+    /* 选中时的颜色 */
 }
 </style>
